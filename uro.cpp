f558216e7a67e0c37cf3253252521921b2641523
yudialfayat <59765710+yudialfayat@users.noreply.github.com>
2020-01-18 21:47:40 +0700

    Membuat kerangka utama.
    Kerangka untuk Tampilan menu dan input dari user

--- uro.cpp
+++ uro.cpp
@@ -1,10 +1,34 @@
 #include<iostream>
 using namespace std;
 
+void calc()
+{
+  //Program Kalkulator;
+  cout<<"Kalkulator"<<endl;
+}
+
+void difr()
+{
+  //Program Turunan Polinom;
+  cout<<"Turunan"<<endl;
+}
+
 int main()
 {
+  cout<<"Program Kalkulator dan Turunan Polinom"<<endl;
+  cout<<"______________________________________"<<endl;
+  cout<<"1. Kalkulator \n" << "2. Turunan \n";
+  cout<<"Masukkan pilihan : ";
 
-  
+  char n;
+  cin>>n; 
+
+  cout<<"______________________________________"<<endl;
+
+  if (n == '1')
+    calc();
+  else
+    difr();
 
   return 0;
 }
